2cda1a830e5f97f7fe4ed4a6ac27df965e6c0920
Remco <remco@rem.codes>
2025-08-05 22:01:48 +0200

    feat: create empty changelog template

--- setup.sh
+++ setup.sh
@@ -372,6 +372,9 @@ EOF
 
 All notable changes to this project will be documented in this file.
 
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
 ## [Unreleased]
 
 ### Added
@@ -385,8 +388,6 @@ All notable changes to this project will be documented in this file.
 ### Deprecated
 
 ### Security
-
-### Maintenance
 EOF
     fi
     
@@ -510,4 +511,4 @@ main() {
 }
 
 # Run main function
-main "$@" # Force cache refresh Tue Aug  5 21:52:01 CEST 2025
+main "$@" 
